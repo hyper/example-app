665fef019fe7d7dde3b627e43002f59dedce0380
Ben Botvinick <benbotvinick@gmail.com>
2021-12-27 17:16:45 -0700

    Fix success page

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -4,7 +4,7 @@ const particlesConfig = require('../constants/particles_config');
 export default function Callback({ account }) {
   return (
     <main>
-      Success! We&apos;ve added snow to {account}. Take a look at
+      Success! We&apos;ve added snow to {account.name}. Take a look at
       your <a href={`https://${account.domain}`}>wintery site</a>, or
       click <a href="https://hyper.co/dashboard">here</a> to return to Hyper.
     </main>
@@ -28,7 +28,10 @@ export async function getServerSideProps({ query }) {
 
     return {
       props: {
-        account: account.name,
+        account: {
+          name: account.name,
+          domain: account.domain,
+        },
       },
     }
   } catch (e) {
